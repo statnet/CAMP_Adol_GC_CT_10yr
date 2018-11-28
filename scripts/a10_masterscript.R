0f9f355d05925810307453ca3bdfb4a1e8faf71a
Steven M. Goodreau <goodreau@uw.edu>
2018-11-27 16:28:12 -0800

    Adds in the a10_masterscript that will be the single script that re-creates the entire project from start to finish.

---
+++ scripts/a10_masterscript.R
@@ -0,0 +1,16 @@
+
+################################################
+# teen-SPARC 10-year extension master script
+# 
+# Notes: 
+#   1. Change the setwd() line if needed
+
+
+### Basics
+setwd("C:/git/CAMP_10yr_proj/scripts/")
+rm(list=ls())
+
+# Get all inputs
+source("CAMP_10yr_import.R")
+
+save.image()
