a70bbcf3b872e21ccfb1c69cea581652fd18b6be
Steven M. Goodreau <goodreau@uw.edu>
2019-01-08 17:30:24 -0800

    All sorts of edits to the a10 function. Basically taking it from a placeholder to a real function

--- R/a10.R
+++ R/a10.R
@@ -4,18 +4,22 @@
 #'
 #' @param n_f A 3x6x11 array indicating the population size for females, by race/eth by age by year
 #' @param n_m A 3x6x11 array indicating the population size for males, by race/eth by age by year
-#' @param sexdeb_f A 3x6x11 matrix inidicating the % of females debuted (as FSM), by race/eth by age by year
-#' @param sexdeb_m A 3x6x11 matrix inidicating the % of males debuted (as MSF), by race/eth by age by year
-#' @param beta_f2m Per-act transmission probability from female to male
-#' @param beta_m2f Per-act transmission probability from male to female
-#' @param init_prev_f A 3x6 matrix indicating the initial STI prevalence among females who have sexually debuted, by race/eth by age
-#' @param init_prev_m A 3x6 matrix indicating the initial STI prevalence among males who have sexually debuted, by race/eth by age
-#' @param newppy_f A 3x6 matrix indicating the mean new partners per year for debuted females, by race/eth by age
-#' @param newppy_m A 3x6 matrix indicating the mean new partners per year for debuted males, by race/eth by age
-#' @param coital_acts_pp_f A 3x6 matrix indicating the mean coital acts per partner for females, by race/eth by age
-#' @param coital_acts_pp_m A 3x6 matrix indicating the mean coital acts per partner for males, by race/eth by age
+#' @param prop_eversex_f A 3x6x11 matrix inidicating the proportion of females debuted (as FSM), by race/eth by age by year
+#' @param prop_eversex_m A 3x6x11 matrix inidicating the proportion of males debuted (as MSF), by race/eth by age by year
 #' @param condom_use_f, A 3x6x11 matrix indicating % condom use for females, by race/eth by age by year
 #' @param condom_use_m, A 3x6x11 matrix indicating % condom use for males, by race/eth by age by year
+#' @param mean_new_part_f A 3x6x11 matrix indicating the mean new partners per year for debuted females, by race/eth by age
+#' @param mean_new_part_m A 3x6x11 matrix indicating the mean new partners per year for debuted males, by race/eth by age
+#' @param coital_acts_pp_f A 3x6x11 matrix indicating the mean coital acts per partner for females, by race/eth by age
+#' @param coital_acts_pp_m A 3x6x11 matrix indicating the mean coital acts per partner for males, by race/eth by age
+#' @param diag_init_f A 3x6 matrix indicating the number of recent annual STI diagnoses among females, by race/eth by age
+#' @param diag_init_m A 3x6 matrix indicating the number of recent annual STI diagnoses among males, by race/eth by age
+#' @param prop_diag_f The proportion of females who get diagnosed for the STI
+#' @param prop_diag_m The proportion of males who get diagnosed for the STI
+#' @param dur_inf_f The average duration of infection for females
+#' @param dur_inf_m The average duration of infection for males
+#' @param beta_f2m Per-act transmission probability from female to male
+#' @param beta_m2f Per-act transmission probability from male to female
 #' 
 #' @return A list comprising two arrays of dimensions [3,6,11], containing the estimated number of 
 #'   incident cases of STI per age per year. The three rows represent the 
@@ -25,67 +29,95 @@
 
 #' @export
 a10 <- function(n_f, n_m, 
-                sexdeb_f, sexdeb_m,
-                init_prev_f, init_prev_m,
-                beta_m2f, beta_f2m,
-                newppy_f, newppy_m,
-                coital_acts_pp_f, coital_acts_pp_m,
+                prop_eversex_f,
+                prop_eversex_m,
+                condom_use_f,
+                condom_use_m,
+                mean_new_part_f,
+                mean_new_part_m,
+                coital_acts_pp_f,
+                coital_acts_pp_m,
+                diag_init_f,
+                diag_init_m,
+                prop_diag_f,
+                prop_diag_m,
+                dur_inf_f,
+                dur_inf_m,
+                beta_f2m,
+                beta_m2f
+        ) {
 
-                                condom_use_f, condom_use_m
-                #ann_chg_npartners, ann_chg_coital, ann_chg_condoms
-       ) {
-  ################################################
+  ##################################################
   # Notes to self
-  # Will need to convert incidence to prevalence again
-  # Will need to incprorate race mixing
+  # Need to convert incidence to prevalence again
+  # Need to incorporate race mixing
   # Need to figure out how to include sex in last 3 mos
   # Need to decide on model calibration
-  # Need to build ppy converter following tool method, or do something different
-  # Need to redo all tool parameters by race/eth
   
   ##########################################################################
   # Dimensional error checking
-  if (dim(n_f) != c(3,6,11)) stop("n_f must be an array with dimensions c(3,6,11).")
-  if (dim(n_m) != c(3,6,11)) stop("n_m must be an array with dimensions c(3,6,11).")
-  # More to do.
+  if (sum(dim(n_f) == c(3,6,11)) <3) stop("n_f must be an array with dimensions c(3,6,11).")
+  if (sum(dim(n_f) == c(3,6,11)) <3) stop("n_m must be an array with dimensions c(3,6,11).")
+  # TODO
   
   ##########################################################################
   # Init bookkeeping
   
-  # Calc # who have seuxally debuted
-  n_sexdeb_f <- n_f * sexdeb_f
-  n_sexdeb_m <- n_m * sexdeb_m
+  # Calc # who have sexually debuted
+  n_eversex_f <- n_f * prop_eversex_f
+  n_eversex_m <- n_m * prop_eversex_m
   
-  # Create arrays to store number of prevalent cases in the cross-section
-  n_prev_f <- n_prev_m <- array(dim=c(3,6,11))
-  n_prev_f[,,1] <- n_sexdeb_f[,,1] * init_prev_f
-  n_prev_m[,,1] <- n_sexdeb_m[,,1] * init_prev_m
-  
+  # Create arrays to store partner prevalence per year
+  part_prev_f <- part_prev_m <- array(dim=c(3,6,10))
+
   # Create arrays to store number of incident cases per year
-  n_inc_f <- n_inc_m <- array(dim=c(3,6,11))
+  n_inc_f <- n_inc_m <- array(dim=c(3,6,10))
   n_inc_f[,,1] <- NA
   n_inc_m[,,1] <- NA
 
-    #
-  cl_acts_f <- newppy_f * coital_acts_pp_f * (1-condom_use_f)
-  cl_acts_m <- newppy_m * coital_acts_pp_m * (1-condom_use_m)
+  # Create arrays to store number of diagnoses per year
+  n_diag_f <- n_diag_m <- array(dim=c(3,6,11))
+  n_diag_f[,,1] <- diag_init_f
+  n_diag_m[,,1] <- diag_init_m
+
+  # Create arrays to store number of prevalent cases in the cross-section
+  n_prev_f <- n_prev_m <- array(dim=c(3,6,11))
+  n_prev_f[,,1] <- diag_init_f * dur_inf_f / prop_diag_f
+  n_prev_m[,,1] <- diag_init_m * dur_inf_m / prop_diag_m
+  
+  # Calculate the number of condomless acts per person
+  cl_acts_f <- mean_new_part_f * coital_acts_pp_f * (1-condom_use_f)
+  cl_acts_m <- mean_new_part_m * coital_acts_pp_m * (1-condom_use_m)
   
   ##########################################################################
   # Advancement
-  
-  for (i in 1:10) {
-    # Sexually debuted
-    #nf_sexdeb[,, i+1] <- cbind(0,nf_sexdeb[, 1:5, i]) + nf[,,1]*pc_debuting_f
-    #nm_sexdeb[,, i+1] <- cbind(0,nm_sexdeb[, 1:5, i]) + nm[,,1]*pc_debuting_m
-  
-  inc_f[,,i+1] <- cl_acts_f[,,i+1] * prev_m[,,i]
+
+  for (i in 1:10) {  
+
+    part_prev_f[,,i] <- matrix(rep(rowSums(n_prev_m[,,i])/rowSums(n_eversex_m[,,i]),6),
+                          nrow=3, byrow=FALSE)
     
-  }
+    part_prev_m[,,i] <- matrix(rep(rowSums(n_prev_f[,,i])/rowSums(n_eversex_f[,,i]),6),
+                          nrow=3, byrow=FALSE)
+    
+    n_inc_f[,,i] <- (n_eversex_f[,,i] - n_prev_f[,,i]) * part_prev_f[,,i] *
+                        cl_acts_f[,,i] * beta_m2f 
+
+    n_inc_m[,,i] <- (n_eversex_m[,,i] - n_prev_m[,,i]) * part_prev_m[,,i] *
+                        cl_acts_m[,,i] * beta_f2m
+
+    n_prev_f[,,i+1] <- n_inc_f[,,i] * dur_inf_f
+    n_prev_m[,,i+1] <- n_inc_m[,,i] * dur_inf_m
+  
+    n_diag_f[,,i+1] <- n_inc_f[,,i] * prop_diag_f
+    n_diag_m[,,i+1] <- n_inc_m[,,i] * prop_diag_m
+    
+    }
   
   
   ##########################################################################
   # Final processing
 
-  result <- nf_sexdeb
+  result <- n_inc_f
   return(result)
 }
