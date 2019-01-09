d2d3ef1031e38ef9269b9d5fa275e52e72e3c8f1
Steven M. Goodreau <goodreau@uw.edu>
2019-01-08 17:31:10 -0800

    tons o work

--- scripts/a10_masterscript.R
+++ scripts/a10_masterscript.R
@@ -1,36 +1,29 @@
 
-################################################
+#########################################################################
 # teen-SPARC 10-year extension master script
 # 
 # Notes: 
 #   1. Change the setwd() line if needed
 
 
+#########################################################################
 ### Basics
+
 setwd("C:/git/CAMP_10yr_proj/scripts/")
 rm(list=ls())
-
-# Get all inputs
-source("a10_import.R")
-
-# Set years info
-years <- seq(2007, 2017, by=2)
+source("a10_import.R")                  # Get all inputs
+years <- seq(2007, 2017, by=2)          # Set years info
 nyears <- length(years)
 
-##### TODO: Using total schoolpops from census for sure.  But:
-#####       for race: we are currently using mean of weights across time 
-#####                    - but this ignores that some of the total are not B/H/W
-#####                 we can find out the wts for the other race/ethn groups, and subtract from census (yes)
-#####                 or we could use the census pop numbers (no)
-#####       for sex: currently we assume 50/50 but should redo with age/specific weights averaged across yrs.
-#####
 
-#### Get pop sizes by age/race/sex that are averaged across years
+#########################################################################
+### HS-attending pop sizes by age/race/sex (averaged across years)
+
 meanschoolpop <- mean(schoolpops$totschoolpop)                 # Tot pop size averaged across years
 mean_pct_age_race <- apply(wts_f+wts_m, 1:2, mean) / 
           sum(apply(wts_f+wts_m, 1:2, mean))                   # Tot % age/race averaged across years
 
-pct_f <- sum(wts_f) / (sum(wts_f) + sum(wts_m))                         # Tot % female averaged across years
+pct_f <- sum(wts_f) / (sum(wts_f) + sum(wts_m))                # Tot % female averaged across years
 
 n_f <- array11(mean_pct_age_race * pct_f * meanschoolpop)
 n_m <- array11(mean_pct_age_race * (1-pct_f) * meanschoolpop)
@@ -40,7 +33,15 @@ n_m <- n_m[-4,,]
 wts_f <- wts_f[-4,,]
 wts_m <- wts_m[-4,,]
 
-#### Get eversex sizes
+#########################################################################
+### Total (HS or not HS) pop sizes by age/race/sex (averaged across years)
+
+# TODO 
+
+
+
+#########################################################################
+### Eversex sizes
 
 prop_eversex_f <- eversex_f / wts_f
 prop_eversex_f[prop_eversex_f==Inf] <- 0
@@ -50,10 +51,14 @@ colnames(prop_eversex_f_df) <- c('ethn', 'age', 'year')
 prop_eversex_f_df$prop_eversex <- as.vector(prop_eversex_f)
 prop_eversex_f_df$wts <- as.vector(wts_f)
 prop_eversex_f_df$agefac <- relevel(as.factor(prop_eversex_f_df$age), ref='16')
-eversex_f_reg <- glm(prop_eversex ~ agefac + year + ethn + year*ethn,
+eversex_f_reg <- suppressWarnings(glm(prop_eversex ~ agefac + year + ethn + year*ethn,
                     data=prop_eversex_f_df, weights=wts, 
-                    family="binomial")
-pred_eversex_f <- array(predict(eversex_f_reg, type='response'), dim=c(3,6,6))
+                    family="binomial"))
+pred_eversex_f_df_indep <- expand.grid(c('B','H','W'), 13:18, 2007:2017)
+colnames(pred_eversex_f_df_indep) <- c('ethn', 'age', 'year')
+pred_eversex_f_df_indep$agefac <- relevel(as.factor(pred_eversex_f_df_indep$age), ref='16')
+pred_eversex_f <- array(predict(eversex_f_reg, type='response', 
+                                newdata= pred_eversex_f_df_indep), dim=c(3,6,11))
 
 prop_eversex_m <- eversex_m / wts_m
 prop_eversex_m[prop_eversex_m==Inf] <- 0
@@ -63,68 +68,50 @@ colnames(prop_eversex_m_df) <- c('ethn', 'age', 'year')
 prop_eversex_m_df$prop_eversex <- as.vector(prop_eversex_m)
 prop_eversex_m_df$wts <- as.vector(wts_m)
 prop_eversex_m_df$agefac <- relevel(as.factor(prop_eversex_m_df$age), ref='16')
-eversex_m_reg <- glm(prop_eversex ~ agefac + year + ethn + year*ethn,
-                     data=prop_eversex_m_df, weights=wts, 
-                     family="binomial")
-pred_eversex_m <- array(predict(eversex_m_reg, type='response'), dim=c(3,6,6))
+eversex_m_reg <- suppressWarnings(glm(prop_eversex ~ agefac + year + ethn + year*ethn,
+                                      data=prop_eversex_f_df, weights=wts, 
+                                      family="binomial"))
+pred_eversex_m_df_indep <- expand.grid(c('B','H','W'), 13:18, 2007:2017)
+colnames(pred_eversex_m_df_indep) <- c('ethn', 'age', 'year')
+pred_eversex_m_df_indep$agefac <- relevel(as.factor(pred_eversex_m_df_indep$age), ref='16')
+pred_eversex_m <- array(predict(eversex_m_reg, type='response', 
+                                newdata= pred_eversex_m_df_indep), dim=c(3,6,11))
+                        
 
-if(F) {
-  matplot(t(pred_eversex_f[,1,]),type='l', ylim=c(0,1))
-  matplot(t(pred_eversex_f[,2,]),type='l', add=T)
-  matplot(t(pred_eversex_f[,3,]),type='l', add=T)
-  matplot(t(pred_eversex_f[,4,]),type='l', add=T)
-  matplot(t(pred_eversex_f[,5,]),type='l', add=T)
-  matplot(t(pred_eversex_f[,6,]),type='l', add=T)
-
-  matplot(t(pred_eversex_m[,1,]),type='l', ylim=c(0,1))
-  matplot(t(pred_eversex_m[,2,]),type='l', add=T)
-  matplot(t(pred_eversex_m[,3,]),type='l', add=T)
-  matplot(t(pred_eversex_m[,4,]),type='l', add=T)
-  matplot(t(pred_eversex_m[,5,]),type='l', add=T)
-  matplot(t(pred_eversex_m[,6,]),type='l', add=T)
-}
-
-################### Condom use
-
-#### Get condomuse sizes
+#########################################################################
+### Condom use
 
 condom_f_df <- expand.grid(c('B','H','W'), 13:18, seq(2007,2017,2))
 colnames(condom_f_df) <- c('ethn', 'age', 'year')
 condom_f_df$condom <- as.vector(condom_f)
 condom_f_df$wts <- as.vector(condom_wts_f)
 condom_f_df$agefac <- relevel(as.factor(condom_f_df$age), ref='16')
-condom_f_reg <- glm(condom ~ agefac + year + ethn + year*ethn,
+condom_f_reg <- suppressWarnings(glm(condom ~ agefac + year + ethn + year*ethn,
                      data=condom_f_df, weights=wts, 
-                     family="binomial")
-pred_condom_f<- array(predict(condom_f_reg, type='response'), dim=c(3,6,6))
+                     family="binomial"))
+pred_condom_f_df_indep <- expand.grid(c('B','H','W'), 13:18, 2007:2017)
+colnames(pred_condom_f_df_indep) <- c('ethn', 'age', 'year')
+pred_condom_f_df_indep$agefac <- relevel(as.factor(pred_condom_f_df_indep$age), ref='16')
+pred_condom_f <- array(predict(condom_f_reg, type='response',
+                                  newdata=pred_condom_f_df_indep), dim=c(3,6,11))
 
 condom_m_df <- expand.grid(c('B','H','W'), 13:18, seq(2007,2017,2))
 colnames(condom_m_df) <- c('ethn', 'age', 'year')
 condom_m_df$condom <- as.vector(condom_m)
 condom_m_df$wts <- as.vector(condom_wts_m)
 condom_m_df$agefac <- relevel(as.factor(condom_m_df$age), ref='16')
-condom_m_reg <- glm(condom ~ agefac + year + ethn + year*ethn,
+condom_m_reg <- suppressWarnings(glm(condom ~ agefac + year + ethn + year*ethn,
                     data=condom_m_df, weights=wts, 
-                    family="binomial")
-pred_condom_m <- array(predict(condom_m_reg, type='response'), dim=c(3,6,6))
+                    family="binomial"))
+pred_condom_m_df_indep <- expand.grid(c('B','H','W'), 13:18, 2007:2017)
+colnames(pred_condom_m_df_indep) <- c('ethn', 'age', 'year')
+pred_condom_m_df_indep$agefac <- relevel(as.factor(pred_condom_m_df_indep$age), ref='16')
+pred_condom_m <- array(predict(condom_m_reg, type='response',
+                                  newdata=pred_condom_m_df_indep), dim=c(3,6,11))
 
-if(F) {
-  matplot(t(pred_condom_f[,1,]),type='l', ylim=c(0,1))
-  matplot(t(pred_condom_f[,2,]),type='l', add=T)
-  matplot(t(pred_condom_f[,3,]),type='l', add=T)
-  matplot(t(pred_condom_f[,4,]),type='l', add=T)
-  matplot(t(pred_condom_f[,5,]),type='l', add=T)
-  matplot(t(pred_condom_f[,6,]),type='l', add=T)
-  
-  matplot(t(pred_condom_m[,1,]),type='l', ylim=c(0,1))
-  matplot(t(pred_condom_m[,2,]),type='l', add=T)
-  matplot(t(pred_condom_m[,3,]),type='l', add=T)
-  matplot(t(pred_condom_m[,4,]),type='l', add=T)
-  matplot(t(pred_condom_m[,5,]),type='l', add=T)
-  matplot(t(pred_condom_m[,6,]),type='l', add=T)
-}
 
-##### Get mnnppy values
+#########################################################################
+### mean # new partners per year (mnnppy)
 
 mnppy_f <- array(dim=c(3,6,6))
 mnppy_wts_f <- array(dim=c(3,6,6))
@@ -162,10 +149,14 @@ mnppy_f_df$mnppy <- as.vector(mnppy_f)
 mnppy_f_df$wts <- as.vector(mnppy_wts_f)
 mnppy_f_df$agefac <- relevel(as.factor(mnppy_f_df$age), ref='16')
 mnppy_f_df$y2k <- mnppy_f_df$year - 2000
-mnppy_f_reg <- glm(mnppy ~ ethn + y2k + ethn*y2k + age + I(age^2),
-                   data=mnppy_f_df, weights=wts, na.action=na.exclude, family="poisson")
-pred_mnppy_f <- array(predict(mnppy_f_reg, type='response', newdata = mnppy_f_df), 
-                      dim=c(3,6,6))
+mnppy_f_reg <- suppressWarnings(glm(mnppy ~ ethn + y2k + ethn*y2k + age + I(age^2),
+                   data=mnppy_f_df, weights=wts, na.action=na.exclude, family="poisson"))
+pred_mnppy_f_df_indep <- expand.grid(c('B','H','W'), 13:18, 2007:2017)
+colnames(pred_mnppy_f_df_indep) <- c('ethn', 'age', 'year')
+pred_mnppy_f_df_indep$agefac <- relevel(as.factor(pred_mnppy_f_df_indep$age), ref='16')
+pred_mnppy_f_df_indep$y2k <- pred_mnppy_f_df_indep$year - 2000
+pred_mnppy_f <- array(predict(mnppy_f_reg, type='response',
+                              newdata=pred_mnppy_f_df_indep), dim=c(3,6,11))
 
 mnppy_m_df <- expand.grid(c('B','H','W'), 13:18, seq(2007,2017,2))
 colnames(mnppy_m_df) <- c('ethn', 'age', 'year')
@@ -173,50 +164,94 @@ mnppy_m_df$mnppy <- as.vector(mnppy_m)
 mnppy_m_df$wts <- as.vector(mnppy_wts_m)
 mnppy_m_df$agefac <- relevel(as.factor(mnppy_m_df$age), ref='16')
 mnppy_m_df$y2k <- mnppy_m_df$year - 2000
-mnppy_m_reg <- glm(mnppy ~ ethn + y2k + ethn*y2k + age + I(age^2),
-                   data=mnppy_m_df, weights=wts, na.action=na.exclude, family="poisson")
-pred_mnppy_m <- array(predict(mnppy_m_reg, type='response', newdata = mnppy_m_df), 
-                      dim=c(3,6,6))
+mnppy_m_reg <- suppressWarnings(glm(mnppy ~ ethn + y2k + ethn*y2k + age + I(age^2),
+                   data=mnppy_m_df, weights=wts, na.action=na.exclude, family="poisson"))
+pred_mnppy_m_df_indep <- expand.grid(c('B','H','W'), 13:18, 2007:2017)
+colnames(pred_mnppy_m_df_indep) <- c('ethn', 'age', 'year')
+pred_mnppy_m_df_indep$agefac <- relevel(as.factor(pred_mnppy_m_df_indep$age), ref='16')
+pred_mnppy_m_df_indep$y2k <- pred_mnppy_m_df_indep$year - 2000
+pred_mnppy_m <- array(predict(mnppy_m_reg, type='response',
+                                 newdata=pred_mnppy_m_df_indep), dim=c(3,6,11))
+
+#########################################################################
+### Coital acts per partner
+
+# TODO 
+capp_f <- array11(mat3(c( 9.4, 9.4, 9.4, 24.7, 24.7, 46.7,
+                          9.4, 9.4, 9.4, 24.7, 24.7, 46.7,
+                          9.4, 9.4, 9.4, 24.7, 24.7, 46.7
+                         )))
+
+capp_m <- array11(mat3(c( 11.9, 11.9, 11.9, 19.3, 19.3, 29.3,
+                          11.9, 11.9, 11.9, 19.3, 19.3, 29.3,
+                          11.9, 11.9, 11.9, 19.3, 19.3, 29.3
+                         )))
+
+#########################################################################
+### Init diagnoses, total (in and out of school)
+
+# TODO 
+
+diagnoses_init_tot_f_gc <- mat3(c(  5,  20,  30,  40,  50,  600,
+                                    5,  30,  45,  60,  75,  900,
+                                    5, 200, 300, 400, 500, 6000
+                         ))
+
+diagnoses_init_tot_m_gc <- mat3(c( 5,  20,  30,  40,  50,  600,
+                                   5,  30,  45,  60,  75,  900,
+                                   5, 200, 300, 400, 500, 6000
+))
 
 
-if(F) {
-  plot(pred_mnppy_f, ylim=c(0,2))
-  points(pred_mnppy_m, col='red')
+#########################################################################
+### Small inputs
 
-  plot(as.vector(pred_mnppy_f[1,,]), ylim=c(0,2), type='l')
-  lines(as.vector(pred_mnppy_m[1,,]), lty=2)
-  lines(as.vector(pred_mnppy_f[2,,]), lty=1, col='blue')
-  lines(as.vector(pred_mnppy_m[2,,]), lty=2, col='blue')
-  lines(as.vector(pred_mnppy_f[3,,]), lty=1, col='red')
-  lines(as.vector(pred_mnppy_m[3,,]), lty=2, col='red')
-  
-  matplot(t((mnppy_f[1,,])), type='l', ylim=c(0,2), col=rainbow(6), lty=2, lwd=0.5)
-  matplot(t(pred_mnppy_f[1,,]), type='l', add=TRUE, col=rainbow(6), lty=1, lwd=1.5)
-  matplot(t((mnppy_f[2,,])), type='l', ylim=c(0,2), col=rainbow(6), lty=2, lwd=0.5)
-  matplot(t(pred_mnppy_f[2,,]), type='l', add=TRUE, col=rainbow(6), lty=1, lwd=1.5)
-  matplot(t((mnppy_f[3,,])), type='l', ylim=c(0,2), col=rainbow(6), lty=2, lwd=0.5)
-  matplot(t(pred_mnppy_f[3,,]), type='l', add=TRUE, col=rainbow(6), lty=1, lwd=1.5)
-  
-}
+beta_rpv_gc <- 0.5856
+beta_ipv_gc <- 0.1164
+
+prop_diag_f_gc <- 0.523
+prop_diag_m_gc <- 0.490
+
+dur_f_gc <- 0.46
+dur_m_gc <- 0.23
 
 
-if(F) {
-  a10_gc01 <- a10(n_f=n_f, n_m=n_m,
-                    init_sexdeb_f=init_sexdeb_f,
-                    init_sexdeb_m=init_sexdeb_m,
-#                    beta_m2f=beta_m2f,
-#                    beta_f2m=beta_f2m,
-                     prop_evesex_f=prop_evesex_f,
-                     prop_evesex_m=prop_evesex_m
-#                    coital_acts_pp_f=coital_acts_pp_f,
-#                    coital_acts_pp_m=coital_acts_pp_m,
-#                    condom_use_f=condom_use_f,
-#                    condom_use_m=condom_use_m,
-#                    ann_chg_npartners=ann_chg_npartners,
-#                    ann_chg_coital=ann_chg_coital,
-#                    ann_chg_condoms=ann_chg_condoms
-  )
-}
+
+#########################################################################
+### Call function to convert all diagnoses to in-school diagnoses
+
+# TODO
+diagnoses_init_sch_f_gc <- diagnoses_init_tot_f_gc
+diagnoses_init_sch_m_gc <- diagnoses_init_tot_m_gc
+
+
+#########################################################################
+### Call main function
+
+#if(F) 
+  a10_gc01 <- a10(n_f = n_f, 
+                n_m = n_m,
+                prop_eversex_f = pred_eversex_f,
+                prop_eversex_m = pred_eversex_m,
+                condom_use_f = pred_condom_f,
+                condom_use_m = pred_condom_m,
+                mean_new_part_f = pred_mnppy_f,
+                mean_new_part_m = pred_mnppy_m,
+                coital_acts_pp_f = capp_f,
+                coital_acts_pp_m = capp_m,
+                diag_init_f = diagnoses_init_tot_f_gc,
+                diag_init_m = diagnoses_init_tot_m_gc,
+                prop_diag_f = prop_diag_f_gc,
+                prop_diag_m = prop_diag_m_gc,
+                dur_inf_f = dur_f_gc,
+                dur_inf_m = dur_m_gc,
+                beta_f2m = beta_ipv_gc,
+                beta_m2f = beta_rpv_gc
+)
+
+
+#########################################################################
+### Process results
 
 # Save image
 save.image()
