a431dd002b68e815a4b8c87da124354ce36aa854
Steven M. Goodreau <goodreau@uw.edu>
2019-02-11 17:29:55 -0800

    Wrote diag_inschool_calc

--- R/diag_inschool_calc.R
+++ R/diag_inschool_calc.R
@@ -3,15 +3,14 @@
 #'Function to convert all diagnoses to diagnoses in school 
 #'  NA: to be run separately by sex
 #'
-#' @param XX A 3x6 matrix of past-year diagnoses among
-#' @param XX A 3x6 matrix of precent of cases that get diagnosed
-#' @param XX A 3x6 matrix indicating mean dur of infection
-#' @return A 3 x 6 matrix indicating est. number of prevalent cases in the cross-section
+#' @param diags A 3x6 matrix of past-year diagnoses among adols regardless of school status
+#' @param perc_inschool A 3x6 matrix of precent of adols in HS
+#' @return A 3x6 matrix indicating number of diagnoses among HS students
 
 #' @export
-diag_inschool_calc <- function(diags, perc_diag, infdur) {
-  initPrev <- (diags/perc_diag)/infdur
-  return(initPrev)
+diag_inschool_calc <- function(diags, perc_inschool) {
+  diags_inschool <- diags*perc_inschool
+  return(diags_inschool)
   }
 
 
