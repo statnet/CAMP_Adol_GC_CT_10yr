8b007ed18865f52fb4149a50e67607ea420554cd
Steven M. Goodreau <goodreau@uw.edu>
2019-02-22 13:23:15 -0800

    Adding in meanpop_tot_x to bring in the total pop (in HS or out); redefining diag_init_f to be just per race, not per race and age, to better match input structure

--- R/a10.R
+++ R/a10.R
@@ -12,14 +12,16 @@
 #' @param mean_new_part_m A 3x6x11 matrix indicating the mean new partners per year for debuted males, by race/eth by age
 #' @param coital_acts_pp_f A 3x6x11 matrix indicating the mean coital acts per partner for females, by race/eth by age
 #' @param coital_acts_pp_m A 3x6x11 matrix indicating the mean coital acts per partner for males, by race/eth by age
-#' @param diag_init_f A 3x6 matrix indicating the number of recent annual STI diagnoses among HS females, by race/eth by age
-#' @param diag_init_m A 3x6 matrix indicating the number of recent annual STI diagnoses among HS males, by race/eth by age
+#' @param diag_init_f A vector of length 3 indicating the number of recent annual STI diagnoses among females, by race/eth
+#' @param diag_init_m A vector of length 3 indicating the number of recent annual STI diagnoses among males, by race/eth
 #' @param prop_diag_f The proportion of females who get diagnosed for the STI
 #' @param prop_diag_m The proportion of males who get diagnosed for the STI
 #' @param dur_inf_f The average duration of infection for females
 #' @param dur_inf_m The average duration of infection for males
 #' @param beta_f2m Per-act transmission probability from female to male
 #' @param beta_m2f Per-act transmission probability from male to female
+#' @param meanpop_tot_f Total female population in and out of school across the relevant ages
+#' @param meanpop_tot_m Total male population in and out of school across the relevant ages
 #' 
 #' @return A list comprising two arrays of dimensions [3,6,11], containing the estimated number of 
 #'   incident cases of STI per age per year. The three rows represent the 
@@ -44,14 +46,15 @@ a10 <- function(n_f, n_m,
                 dur_inf_f,
                 dur_inf_m,
                 beta_f2m,
-                beta_m2f
+                beta_m2f,
+                meanpop_tot_f,
+                meanpop_tot_m
         ) {
 
   ##################################################
   # Notes to self
   # Need to convert incidence to prevalence again
   # Need to incorporate race mixing
-  # Need to figure out how to include sex in last 3 mos
   # Need to decide on model calibration
   
   ##########################################################################
@@ -67,23 +70,23 @@ a10 <- function(n_f, n_m,
   n_eversex_f <- n_f * prop_eversex_f
   n_eversex_m <- n_m * prop_eversex_m
   
-  # Create arrays to store partner prevalence per year
-  part_prev_f <- part_prev_m <- array(dim=c(3,6,10))
-
   # Create arrays to store number of incident cases per year
   n_inc_f <- n_inc_m <- array(dim=c(3,6,10))
   n_inc_f[,,1] <- NA
   n_inc_m[,,1] <- NA
 
-  # Create arrays to store number of diagnoses per year
-  n_diag_f <- n_diag_m <- array(dim=c(3,6,11))
-  n_diag_f[,,1] <- diag_init_f
-  n_diag_m[,,1] <- diag_init_m
+  # Create arrays to store number of diagnoses per year *total* (in and out of HS)
+  #n_diag_tot_f <- n_diag_tot_m <- array(dim=c(3,6,11))
+  #n_diag_tot_f[,,1] <- diag_init_f
+  #n_diag_tot_m[,,1] <- diag_init_m
 
-  # Create arrays to store number of prevalent cases in the cross-section
-  n_prev_f <- n_prev_m <- array(dim=c(3,6,11))
-  n_prev_f[,,1] <- diag_init_f * dur_inf_f / prop_diag_f
-  n_prev_m[,,1] <- diag_init_m * dur_inf_m / prop_diag_m
+  # Create arrays to store number of diagnoses per year *in HS*
+  n_diag_f <- n_diag_m <- array(dim=c(3,6,11))
+  
+  # Create arrays to store prevalence in the cross-section
+  prev_f <- prev_m <- array(dim=c(3,6,11))
+  prev_f[,,1] <- diag_init_f * dur_inf_f / prop_diag_f / rowSums(prop_eversex_f[,,1]*meanpop_tot_f[,,1])
+  prev_m[,,1] <- diag_init_m * dur_inf_m / prop_diag_m / rowSums(prop_eversex_m[,,1]*meanpop_tot_m[,,1])
   
   # Calculate the number of condomless acts per person
   cl_acts_f <- mean_new_part_f * coital_acts_pp_f * (1-condom_use_f)
@@ -94,25 +97,20 @@ a10 <- function(n_f, n_m,
 
   for (i in 1:10) {  
 
-    part_prev_f[,,i] <- matrix(rep(rowSums(n_prev_m[,,i])/rowSums(n_eversex_m[,,i]),6),
-                          nrow=3, byrow=FALSE)
+    if (i>1) prev_f[,,i] <- n_inc_f[,,i-1] * dur_inf_f
+    if (i>1) prev_m[,,i] <- n_inc_m[,,i-1] * dur_inf_m
     
-    part_prev_m[,,i] <- matrix(rep(rowSums(n_prev_f[,,i])/rowSums(n_eversex_f[,,i]),6),
-                          nrow=3, byrow=FALSE)
-    
-    n_inc_f[,,i] <- (n_eversex_f[,,i] - n_prev_f[,,i]) * part_prev_f[,,i] *
+    n_inc_f[,,i] <- n_eversex_f[,,i] * (1-prev_f[,,i]) * prev_m[,,i] *
                         cl_acts_f[,,i] * beta_m2f 
 
-    n_inc_m[,,i] <- (n_eversex_m[,,i] - n_prev_m[,,i]) * part_prev_m[,,i] *
+    n_inc_m[,,i] <- n_eversex_m[,,i] * (1-prev_m[,,i]) * prev_f[,,i] *
                         cl_acts_m[,,i] * beta_f2m
 
-    n_prev_f[,,i+1] <- n_inc_f[,,i] * dur_inf_f
-    n_prev_m[,,i+1] <- n_inc_m[,,i] * dur_inf_m
-  
-    n_diag_f[,,i+1] <- n_inc_f[,,i] * prop_diag_f
-    n_diag_m[,,i+1] <- n_inc_m[,,i] * prop_diag_m
     
-    }
+    n_diag_f[,,i] <- n_inc_f[,,i] * prop_diag_f
+    n_diag_m[,,i] <- n_inc_m[,,i] * prop_diag_m
+    
+  }
   
   
   ##########################################################################
@@ -120,8 +118,8 @@ a10 <- function(n_f, n_m,
 
   result <- list(n_inc_f = n_inc_f, 
                  n_inc_m = n_inc_m, 
-                 n_prev_f = n_prev_f, 
-                 n_prev_m = n_prev_m, 
+                 prev_f = prev_f, 
+                 prev_m = prev_m, 
                  n_diag_f = n_diag_f, 
                  n_diag_m = n_diag_m,
                  n_eversex_f = n_eversex_f, 
