8ba7d9aa7399b5af763338355586cf680655d9b4
Steven M. Goodreau <goodreau@uw.edu>
2019-03-26 16:28:52 -0700

    Adding in p_ethn (call and numbers) to a10_cmomp_with_excel_gc).  These should not have changed the result, since this scenairo only has one ethn group, and indeed they did not.

--- scripts/a10__comp_with_excel_gc.R
+++ scripts/a10__comp_with_excel_gc.R
@@ -41,6 +41,8 @@ capp_m <- array11(mat3(c(
           rep(11.9, 3), rep(19.3, 2), 29.3, rep(0,12)
         )))
 
+p_ethn_f <- p_ethn_m <- mat3(c(1,0,0,0,1,0,0,0,1))
+
 beta_rpv_gc <- 0.58556
 beta_ipv_gc <- 0.11638
 
@@ -72,6 +74,8 @@ a10_gc_sync <- a10(n_f = n_f,
                 mean_new_part_m = pred_mnppy_m,
                 coital_acts_pp_f = capp_f,
                 coital_acts_pp_m = capp_m,
+                p_ethn_f = p_ethn_f,
+                p_ethn_m = p_ethn_m,
                 diag_init_f = diagnoses_init_f_gc,
                 diag_init_m = diagnoses_init_m_gc,
                 prop_diag_f = prop_diag_f_gc,
