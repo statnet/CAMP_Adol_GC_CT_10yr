0d70c7ef15825660bf3a49413f6f6c95ae798690
Steven M. Goodreau <goodreau@uw.edu>
2019-08-21 16:25:39 -0700

    adding calls to CT scripts to "entire project" master file

--- scripts/a10_gcct_entire_project.R
+++ scripts/a10_gcct_entire_project.R
@@ -16,6 +16,7 @@ source("a10_process_inputs.R")                  # Process inputs (i.e. conduct r
 source("a10_make_behav_inputs_all_2007.R")      # override 2009-2017 numbers with 2007 for both calibration and no-behavior-change models
 source("a10_process_dx_for_calib.R")            # create total (pseudo-age-specific) diagnosis numbers for step 1 of the calibration 
 source("a10_ABC_gc.R")
+source("a10_ABC_ct.R")
 
 ########################################################################
 ### Calibrate GC
@@ -41,16 +42,42 @@ source("a10_gc_calibration_pt2_sim.R")          # GC calib pt 2 (starting with n
 
 
 ########################################################################
-### Run GC scenarios
+### Run GC scenarios and credible intervals
 
 source("a10_gc_no_behav_change_script.R")       # No behavior change
 source("a10_gc_obs_behav_change.R")             # Observed behavior change
-
-########################################################################
-### Run credible interval analysis for GC
-
 source("a10_gc_credible_intervals.R")             # Observed behavior change
 
+
+########################################################################
+### Calibrate CT
+
+source("a10_ct_calibration_pt1.R")              # CT calib pt 1 (starting with non-age-specific dx)
+
+source("a10_ct_calibration_ABC_check.R")        # Load calibration check function
+boxplot(a10_calib_ct_pt1$param)                 # Check pt 1 calibration
+calib_test_ct(a10_calib_ct_pt1, 
+              "../output/a10_calib_test_ct_step1_f.pdf", 
+              "../output/a10_calib_test_ct_step1_m.pdf")
+
+source("a10_ct_calibration_pt1_sim.R")          # simulate from CT calib pt 1 to get starting age-specific dx
+
+source("a10_ct_calibration_pt2.R")              # CT calib pt 2 (starting with age-specific dx)
+
+boxplot(a10_calib_ct_pt2$param)                 # Check pt 2 calibration
+calib_test_ct(a10_calib_ct_pt2, 
+              "../output/a10_calib_test_ct_step2_f.pdf", 
+              "../output/a10_calib_test_ct_step2_m.pdf")
+
+source("a10_ct_calibration_pt2_sim.R")          # CT calib pt 2 (starting with non-age-specific dx)
+
+
+########################################################################
+### Run CT scenarios and credible intervals
+
+source("a10_ct_no_behav_change_script.R")       # No behavior change
+source("a10_ct_obs_behav_change.R")             # Observed behavior change
+source("a10_ct_credible_intervals.R")             # Observed behavior change
 
 ########################################################
 ## Generate results for paper 
