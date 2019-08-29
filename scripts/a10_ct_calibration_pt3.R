97751f010c0aefeac232a3f4df4a19f610562748
Steven M. Goodreau <goodreau@uw.edu>
2019-08-29 15:06:22 -0700

    adding ct3

---
+++ scripts/a10_ct_calibration_pt3.R
@@ -0,0 +1,34 @@
+##########################################################
+## CT calibration pt 3
+
+##########################################################
+## Set diagnoses by age for next stage; correct it for any 
+## discrepancy between total from the original nad the ABC
+
+correction_f <- dx_ct_init_tot_f/
+  rowSums(a10_calib_ct_pt2_sim$n_diag_total_f[,,max(cal_times)])
+dx_ct_init_tot_f <- correction_f *
+  a10_calib_ct_pt2_sim$n_diag_total_f[,,max(cal_times)]
+
+correction_m <- dx_ct_init_tot_m/
+  rowSums(a10_calib_ct_pt2_sim$n_diag_total_m[,,max(cal_times)])
+dx_ct_init_tot_m <- correction_m *
+  a10_calib_ct_pt2_sim$n_diag_total_m[,,max(cal_times)]
+
+
+###  Could choose to change priors and tolerance for CT here if necessary
+
+calib_ct_pt3_tolerance <- calib_ct_pt1_tolerance
+calib_ct_pt3_priors <- calib_ct_pt1_priors 
+
+### ABC run
+a10_calib_ct_pt3 <- ABC_sequential(method="Beaumont",
+                           model=a10_ABC_ct,
+                           prior=calib_ct_pt3_priors,
+                           nb_simul=100,
+                           summary_stat_target=0,
+                           tolerance_tab=calib_ct_pt3_tolerance,
+                           verbose=TRUE,
+                           progress_bar=TRUE)
+
+save(a10_calib_ct_pt3, file='../output/a10_calib_ct_pt3.rda')
